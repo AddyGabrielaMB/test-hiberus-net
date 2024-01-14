209c4ae0fb32855b1663b0bf85bd6b752e6a0d74
Addy Gabriela Márquez Briceño <addymarquezb@gmail.com>
2024-01-14 21:10:28 +0100

    Tests

---
+++ Controllers/TestsController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using System.Collections;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
+
+namespace TestHiberusNet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestsController : ControllerBase
+    {
+        public TestsController(){}
+
+        [HttpGet("stringInversion")]
+        public string GetInversion(string text)
+        {
+            if (String.IsNullOrEmpty(text)) return "";
+            char[] charArray = text.ToCharArray();
+            Array.Reverse(charArray);
+            return new String(charArray);
+        }
+
+        [HttpGet("characterRepeated")]
+        public List<KeyValuePair<string, int>> GetRepeated(string text)
+        {
+            var charList = new List<KeyValuePair<string, int>>();
+            char[] charArray = text.ToCharArray();
+
+            foreach (char c in charArray)
+            {
+                int charIndex = charList.FindIndex(e => e.Key.Equals(c.ToString()));
+                if(charIndex < 0)
+                {
+                    charList.Add(new KeyValuePair<string, int>(c.ToString(), 1));
+                    continue;
+                }
+                charList[charIndex] = new KeyValuePair<string, int>(c.ToString(), charList[charIndex].Value + 1);
+            }
+            return charList;
+        }
+
+        [HttpGet("wordQuantity")]
+        public int GetQuantity(string text)
+        {
+            if (String.IsNullOrEmpty(text)) return 0;
+            return text
+                    .Split(" ")
+                    .Where(e => !e.Any(char.IsDigit) && !String.IsNullOrEmpty(e))
+                    .ToArray().Length;
+        }
+
+        [HttpGet("posibleCombinations")]
+        public List<int[]> GetCombinations([FromQuery]List<int>numbers, int finalValue)
+        {
+            List<int[]> combinations = new List<int[]>();
+            List<int> combination = new List<int>();
+            List<int> indexUsed = new List<int>();
+            numbers = numbers.OrderBy(n => n).ToList();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == finalValue)
+                {
+                    combination.Add(numbers[i]);
+                    if (!combinations.Exists(c => c.SequenceEqual(combination))) combinations.Add(combination.ToArray());
+                    combination.Clear();
+                    continue;
+                }
+                else if (numbers[i] < finalValue)
+                {
+                    bool isChecking = false;
+                    combination.Add(numbers[i]);
+                    indexUsed.Add(i);
+                    do
+                    {
+                        var found = numbers.Select((value, index) => new { val = value, i = index })
+                                            .Where(number => number.i > indexUsed.Last() && (combination.Sum() + number.val) <= finalValue)
+                                            .FirstOrDefault();
+
+                        if (found != null)
+                        {
+                            if ((combination.Sum() + found.val) == finalValue)
+                            {
+                                combination.Add(found.val);
+                                if (!combinations.Exists(c => c.SequenceEqual(combination))){
+                                    combinations.Add(combination.ToArray());
+                                    indexUsed.Add(found.i);
+                                };
+                                combination.Clear();
+                                combination.Add(numbers[i]);
+
+                            }else if ((combination.Sum() + found.val) < finalValue)
+                            {
+                                combination.Add(found.val);
+                                indexUsed.Add(found.i);
+                            }
+                        }
+                        else
+                        {
+                            isChecking = true;
+                            indexUsed.Clear();
+                            combination.Clear();
+                        }
+
+                    } while (!isChecking);
+                }
+            }
+            return combinations;
+        }
+    }
+}
